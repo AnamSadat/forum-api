c38ed4a8a19bed41f80af402f1f2523e766e6b75
AnamSadat <anamsadat3@gmail.com>
2026-06-18 13:13:47 +0700

    chore: update environment configuration

--- src/Commons/config.js
+++ src/Commons/config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 if (process.env.NODE_ENV === 'test') {
   dotenv.config({
-    path: path.resolve(process.cwd(), '.test.env'),
+    path: path.resolve(process.cwd(), '.env.test'),
   });
 } else {
   dotenv.config();
