98f173ed80a8563cc4068d15bf2e7443cd830a32
AnamSadat <anamsadat3@gmail.com>
2026-06-21 19:26:20 +0700

    feat(user): validation length

--- src/Domains/users/entities/RegisterUser.js
+++ src/Domains/users/entities/RegisterUser.js
@@ -14,12 +14,12 @@ class RegisterUser {
       throw new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY');
     }
 
-    if (typeof username !== 'string' || typeof password !== 'string' || typeof fullname !== 'string') {
+    if (
+      typeof username !== 'string' ||
+      typeof password !== 'string' ||
+      typeof fullname !== 'string'
+    ) {
       throw new Error('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION');
-    }
-
-    if (username.length > 50) {
-      throw new Error('REGISTER_USER.USERNAME_LIMIT_CHAR');
     }
 
     if (!username.match(/^[\w]+$/)) {
